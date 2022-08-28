807642453eeec4c50ce7bc7841097c5155319a58
Alain <ark2784@gmail.com>
2022-08-28 18:41:31 -0500

    cuarta

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -2,5 +2,12 @@ import React from 'react';
 import '../../App.css';
 
 export default function Shop() {
-  return <h1 className='shop'>SHOP</h1>;
+  return 
+  <>
+  
+    <h1 className='shop'>BUILD YOUR EMOKE</h1>
+  
+  </>;
+    
+  
 }
