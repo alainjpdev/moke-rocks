2082afa58aaa59d768d4eaa9f16ad5c81a0ce192
Alain <ark2784@gmail.com>
2022-08-30 21:46:24 -0500

    946

---
+++ src/components/BodyColors.js
@@ -0,0 +1,29 @@
+
+
+export const ColorsData = [{
+    color:"BLACK",background:"black"
+},{
+    color:"BLUE", background:"blue"
+},{
+    color:"PINK",background:"pink"
+}, {
+    color:"ORANGE",background:"orange"
+},{
+    color: "RED",background:"red"
+},{
+    color:"WHITE",background:"white"
+},{
+    color:"YELLOW",background:"yellow"
+},{
+    color:"GREEN",background:"green"
+},{
+    color:"ROSE",background:"#CEB4BE"
+},{
+    color:"CREAM", background:"#F0F0EF"
+},{
+    color:"MINT", background: "#2CB695"
+},{
+    color:"COBALT", background:"#5591c9"
+},{
+    color:"CAMO+1,100", background:"#828382"
+}]
--- src/components/ColorPallete.js
+++ src/components/ColorPallete.js
@@ -1,30 +1,20 @@
 import React, { useState } from 'react'
+import { ColorsData } from './BodyColors';
 import "./ColorPallete.css"
-const black = "black";
-// const blue = "blue";
-// const pink = "pink"
-// const orange = "orange"
-// const red = "red"
 
-
+const body = "body"
 
 function ColorPallete() {
-    const mokes = [
-    `./images/moke_custom/body/${black}-body.png`,
-    "./images/moke_custom/body/blue-body.png",
-    "./images/moke_custom/body/pink-body.png",
-    "./images/moke_custom/body/orange-body.png",
-    "./images/moke_custom/body/red-body.png",
-    "./images/moke_custom/body/white-body.png",
-    "./images/moke_custom/body/yellow-body.png",
-    "./images/moke_custom/body/cobalt-body.png",
-    "./images/moke_custom/body/green-body.png",
-    "./images/moke_custom/body/rose-body.png",
-    "./images/moke_custom/body/cream-body.png",
-    "./images/moke_custom/body/mint-body.png",
-    "./images/moke_custom/body/camo-body.png"
-    ]
-    const [images, setImages] = useState("./images/moke_custom/body/black-body.png")
+   
+
+    const afterClick = (e) =>{
+
+      console.log(e.target.id)
+      setImages("/images/moke_custom/"+e.target.id+".png")
+      
+    }
+
+    const [image, setImages] = useState("./images/moke_custom/black-body.png")
 
     const [show, setShow] = useState(true)
     
@@ -33,7 +23,7 @@ function ColorPallete() {
   <div className="container">
 
       <div className="container-image">
-        <img src={images} alt="Travel" width={"100%"}/>    
+        <img src={image} alt="Travel" width={"100%"}/>    
       </div>
 
       {show? 
@@ -44,91 +34,20 @@ function ColorPallete() {
        
       <div className="container-moke">
 
-      <div className="card-moke" key={"black"}>
-              <div className="box-moke" onClick={() => 
-                setImages("/images/moke_custom/body/black-body.png")} 
-                style={{background : black}}>
+      {ColorsData.map((color,background) =>{
+            return(
+              <div className="card-moke">
+              <div className="box-moke"
+                key={color.color+body}
+                id={color.color+"-"+body}  
+                onClick={afterClick}
+                style={{background : color.background}}>
+              </div>
+            <p>{color.color}</p>
+          </div>  
                 
-              </div>
-            <p>BLACK</p>
-          </div>  
-
-          <div className="card-moke" key={"blue"}>
-              <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/blue-body.png")} style={{background : "#62B9D4"}}>
-              </div>
-              <p>BLUE</p>
-          </div>  
-
-          <div className="card-moke" key={"pink"}>
-              <div className="box-moke" onClick={() =>
-                 setImages("/images/moke_custom/body/pink-body.png")} 
-                    style={{background : "#FFCBFF"}}>
-              </div>
-              <p>PINK</p>
-          </div>
-
-          <div className="card-moke" key={"orange"}>
-                <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/orange-body.png")} style={{background : "orange"}}>
-                </div>
-                <p>ORANGE</p>
-          </div>  
-
-
-        <div className="card-moke" key={"red"}>
-          <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/red-body.png")} style={{background : "red"}}>
-          </div>
-          <p>RED</p>
-        </div>  
-        
-        <div className="card-moke" key={"white"}>
-            <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/white-body.png")} style={{background : "white"}}>
-            </div>
-            <p>WHITE</p>
-        </div>  
-
-        <div className="card-moke" key={"yellow"}>
-           <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/yellow-body.png")} style={{background : "yellow"}}>
-           </div>
-           <p>YELLOW</p>
-        </div>  
-
-        <div className="card-moke" key={"cobalt"}>
-            <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/cobalt-body.png")} style={{background : "#4A85BD"}}>
-            </div>
-            <p>COBALT</p>
-        </div>  
-
-
-        <div className="card-moke" key={"green"}>
-          <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/green-body.png")} style={{background : "green"}}>
-          </div>
-          <p>GREEN</p>
-        </div>
-
-        <div className="card-moke" key={"rose"}>
-            <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/rose-body.png")} style={{background : "#CEB4BE"}}>
-            </div>
-            <p>ROSE</p>
-        </div>  
-
-        <div className="card-moke" key={"cream"}>
-           <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/cream-body.png")} style={{background : "#F0F0EF"}}>
-           </div>
-           <p>CREAM</p>
-        </div>  
-
-        <div className="card-moke" key={"mint"}>
-            <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/mint-body.png")} style={{background : "#2CB695"}}>
-            </div>
-            <p>MINT</p>
-        </div>  
-
-
-        <div className="card-moke" key={"camo"}>
-          <div className="box-moke" onClick={() => setImages("/images/moke_custom/body/camo-body.png")} style={{background : "#828382"}}>
-          </div>
-          <p>CAMO+$1,100</p>
-        </div>
+                )
+              })}
         </div>
 
       </div>:null}
@@ -138,7 +57,7 @@ function ColorPallete() {
             <a href="#" class="previous" onClick={() => 
               setShow(true)}>&laquo; Previous</a>
 
-              
+
             <a href="#" class="next" onClick={() => setShow(false)}>Next &raquo;</a>
         </div>
     </>
