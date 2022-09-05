9242a00d46733fcf38b4ff986f133a58bdf005a7
Alain <ark2784@gmail.com>
2022-09-05 17:00:01 -0500

    459

--- src/components/ColorPallete.js
+++ src/components/ColorPallete.js
@@ -51,8 +51,8 @@ function ColorPallete() {
       <> 
     <div className="default-car-container">        
         <img className="image2" src="./images/moke pics/body/b-base.png" width={"300px"}/>
-        <img className="image2" src={`./images/moke pics/body/b-${body}.png`} width={"300px"}/>
         <img className="image2" src={`./images/moke pics/grill/g-${grill}.png`} width={"300px"}/>
+        <img className="image2" src={`./images/moke pics/body/b-${body}.png`} width={"300px"}/>
         <img className="image2" src={`./images/moke pics/roll/r-${rollbar}.png`} width={"300px"}/>
         <img className="image2" src="./images/moke pics/seat/s-test.png" width={"300px"}/>
         <img className="image2" src="./images/moke pics/wheel/w-black.png" width={"300px"}/>
